4515074732622dddab90bd1aa2ed89a567c30712
mcemolcusenler <cem.olcusenler@gmail.com>
2017-10-17 15:17:31 +0000

    adds a reactive counter

--- public/scripts/composer-char-counting.js
+++ public/scripts/composer-char-counting.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
   $("form > textarea").on("keyup", function() {
     var charsCount = $(this).val().length;
     var charsLeft = 140 - charsCount;
-    $('.counter').text(charsLeft);
+    var counter = $(this).closest($("form")).find($("span"));
+    counter.text(charsLeft);
+    if (charsLeft < 0) {
+      counter.addClass("counter-red");
+    } else {
+      counter.removeClass("counter-red");
+    }
   });
 });
 
