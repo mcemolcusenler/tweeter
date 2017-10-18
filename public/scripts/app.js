e866c15b88ee6d58d83a6b0a8dfbefe701bc1d0b
mcemolcusenler <cem.olcusenler@gmail.com>
2017-10-18 01:15:49 +0000

    adds functionality to update the page from an array of objects

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,96 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+//the function below assembles and returns article for each object in our data array
+var createTextElement = function (twitObject) {
+  var $article = $('<article>');
+  var $header = $('<header>');
+  var $userAvatar = twitObject.user.avatars.regular;
+  var $image = $(`<img src="${$userAvatar}">`);
+  var $userName = twitObject.user.name;
+  var $pUserName = $(`<p class="user-name">${$userName}</p>`);
+  var $userHandle = twitObject.user.handle;
+  var $pUserHandle = $(`<p class="user-handle">${$userHandle}</p>`);
+  var $userContent = twitObject.content.text;
+  var $pContent = $(`<p>${$userContent}</p>`);
+  var $footer = $('<footer>');
+  var $createdAt = twitObject.created_at;
+  var $pCreatedAt = $(`<p>${$createdAt}</p>`);
+  var $iFlag = $('<i class="fa fa-flag" aria-hidden="true"></i>');
+  var $iRetweet = $('<i class="fa fa-retweet" aria-hidden="true"></i>');
+  var $iHeart = $('<i class="fa fa-heart" aria-hidden="true"></i>');
 
+  $header.append($image);
+  $header.append($pUserName);
+  $header.append($pUserHandle);
+  $article.append($header);
+  //above the header is made up. image, username and handle are appended to header which is appended to article.
+  $article.append($pContent);
+  //below the footer is made up. icons and timestamp are appended to footer which is appended to article.
+  $footer.append($pCreatedAt);
+  $footer.append($iFlag);
+  $footer.append($iRetweet);
+  $footer.append($iHeart);
+  $article.append($footer);
+
+  return $article;
+}
+
+//the function below will loop thru the array, and each article to our index.html
+var renderTweet = function (twitArray) {
+  var $twitOutput = twitArray.map(createTextElement);
+  return $twitOutput;
+}
+
+$(document).ready(function() {
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+  var $storeTweet = renderTweet(data);
+
+  var $sectionAllTweets = $("#all-tweets");
+  $sectionAllTweets.append($storeTweet);
+});
